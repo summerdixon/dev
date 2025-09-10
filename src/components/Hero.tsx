8f88c6a7724751680ed14a84a69d74e73feef280
summerdixon <summerdixon2023@g.ucla.edu>
2025-09-09 23:34:17 -0700

    Moving arrow

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,26 @@
 import '../styles/global.css';
 
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { Box, IconButton, Typography } from '@mui/material';
+import { Circle } from '@mui/icons-material';
 
 export default function Hero() {
+    const [showImage, setShowImage] = React.useState(false);
+
+    const handleClick = () => setShowImage(true);
+
     return (
         <Box className="heroContainer">
             <img src="/landscape-window.svg" className="centeredImage"></img>
             <Typography className="title"> Summer Dixon </Typography>
+            <IconButton 
+                className="xButton"
+                onClick={handleClick}
+            >
+                <Circle/>
+            </IconButton>
+            <img src="/arrow-cursor.svg" alt="" className="movingArrow anim"/>
+            {showImage && <img src="/memoji.svg" alt="" className="memoji"/>}
         </Box>
     );
 }
